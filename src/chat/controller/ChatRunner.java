21f7c175616c1030db320fe7cf8794735a779e88
Ethan throndsen <Ethunderson1@gmail.com>
2015-10-21 13:33:36 -0600

    Added Popup class to View Package

--- src/chat/controller/ChatRunner.java
+++ src/chat/controller/ChatRunner.java
@@ -1,5 +1,15 @@
 package chat.controller;
-
-public class ChatRunner {
+/**
+ * Starts the Chatbot program.
+ * @author ethr5627
+ * @version 1.0 10/21/2015
+ */
+public class ChatRunner 
+{
+	public static void main(String [] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
 
 }
---
+++ src/chat/view/ChatPopup.java
@@ -0,0 +1,34 @@
+package chat.view;
+
+import javax.swing.JOptionPane;
+/**
+ * This class provides Chatbot Responses in PopupBox.
+ * @author ethr5627
+ * @version 1.0 10/21/2015
+ */
+public class ChatPopup
+ 
+{
+	/**
+	 * Display a GUI Popup for collecting user text.
+	 * @param displayMessage
+	 * @return
+	 */
+	public String getAnswer(String displayMessage)
+	{
+		String answer = "";
+		
+		answer = JOptionPane.showInputDialog(null,displayMessage);
+		
+		return answer;
+	}
+	/**
+	 * Shows a Message Dialog in a popup in a textbox.
+	 * @param displayMessage
+	 */
+	public void displayResponse(String displayMessage)
+	{
+		JOptionPane.showMessageDialog(null, displayMessage);
+	}
+	
+}
